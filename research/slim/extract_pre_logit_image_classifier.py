150ee88446e12e95ae8254bce5f9efbaa2ba5556
Ziqiang Feng <zf@cs.cmu.edu>
2017-11-10 21:15:30 -0500

    checkpoint: can extract pre-logit features from a input dir

--- research/slim/extract_pre_logit_image_classifier.py
+++ research/slim/extract_pre_logit_image_classifier.py
@@ -24,21 +24,16 @@ import os
 import sys
 
 import tensorflow as tf
-# import redis
+import pickle
 
 from datasets import dataset_factory
 from nets import nets_factory
 from preprocessing import preprocessing_factory
 
-
 slim = tf.contrib.slim
 
 tf.app.flags.DEFINE_integer(
     'batch_size', 100, 'The number of samples in each batch.')
-
-tf.app.flags.DEFINE_integer(
-    'max_num_batches', None,
-    'Max number of batches to evaluate by default use all.')
 
 tf.app.flags.DEFINE_string(
     'master', '', 'The address of the TensorFlow master to use.')
@@ -48,21 +43,9 @@ tf.app.flags.DEFINE_string(
     'The directory where the model was written to or an absolute path to a '
     'checkpoint file.')
 
-tf.app.flags.DEFINE_string(
-    'eval_dir', '/tmp/tfmodel/', 'Directory where the results are saved to.')
-
 tf.app.flags.DEFINE_integer(
     'num_preprocessing_threads', 4,
     'The number of threads used to create the batches.')
-
-tf.app.flags.DEFINE_string(
-    'dataset_name', 'imagenet', 'The name of the dataset to load.')
-
-tf.app.flags.DEFINE_string(
-    'dataset_split_name', 'test', 'The name of the train/test split.')
-
-tf.app.flags.DEFINE_string(
-    'dataset_dir', None, 'The directory where the dataset files are stored.')
 
 tf.app.flags.DEFINE_integer(
     'labels_offset', 0,
@@ -75,7 +58,7 @@ tf.app.flags.DEFINE_string(
 
 tf.app.flags.DEFINE_string(
     'preprocessing_name', None, 'The name of the preprocessing to use. If left '
-    'as `None`, then the model_name flag is used.')
+                                'as `None`, then the model_name flag is used.')
 
 tf.app.flags.DEFINE_integer(
     'eval_image_size', None, 'Eval image size')
@@ -83,128 +66,131 @@ tf.app.flags.DEFINE_integer(
 tf.app.flags.DEFINE_string(
     'input_dir', None, 'Test image input dir')
 
+tf.app.flags.DEFINE_string(
+    'output_file', None, "Name of output pickle file {'X': X, 'tags', tags'} "
+)
+
 FLAGS = tf.app.flags.FLAGS
 
 
 def main(_):
-  # if not FLAGS.dataset_dir:
-  #   raise ValueError('You must supply the dataset directory with --dataset_dir')
+    if not FLAGS.input_dir:
+        raise ValueError('You must supply the dataset directory with --input_dir')
 
-  tf.logging.set_verbosity(tf.logging.INFO)
-  with tf.Graph().as_default():
+    if not FLAGS.output_file:
+        raise ValueError('You must supply the output file with --output_file')
 
-    ######################
-    # Select the dataset #
-    ######################
-    dataset_wrapper = dataset_factory.get_dataset(
-        FLAGS.dataset_name, FLAGS.dataset_split_name, FLAGS.dataset_dir)
+    tf.logging.set_verbosity(tf.logging.INFO)
+    with tf.Graph().as_default():
 
-    ####################
-    # Select the model #
-    ####################
-    # XXX set num_classes=None to get the pre-logit layers
-    network_fn = nets_factory.get_network_fn(
-        FLAGS.model_name,
-        num_classes=None,
-        is_training=False)
+        ####################
+        # Select the model #
+        ####################
+        # XXX set num_classes=None to get the pre-logit layers
+        network_fn = nets_factory.get_network_fn(
+            FLAGS.model_name,
+            num_classes=None,
+            is_training=False)
 
-    #####################################
-    # Select the preprocessing function #
-    #####################################
-    preprocessing_name = FLAGS.preprocessing_name or FLAGS.model_name
-    image_preprocessing_fn = preprocessing_factory.get_preprocessing(
-        preprocessing_name,
-        is_training=False)
+        #####################################
+        # Select the preprocessing function #
+        #####################################
+        preprocessing_name = FLAGS.preprocessing_name or FLAGS.model_name
+        image_preprocessing_fn = preprocessing_factory.get_preprocessing(
+            preprocessing_name,
+            is_training=False)
 
-    eval_image_size = FLAGS.eval_image_size or network_fn.default_image_size
+        eval_image_size = FLAGS.eval_image_size or network_fn.default_image_size
 
-    def fixed_image_size_preprocessing_fn(image):
-      return image_preprocessing_fn(image, eval_image_size, eval_image_size)
+        def fixed_image_size_preprocessing_fn(image):
+            return image_preprocessing_fn(image, eval_image_size, eval_image_size)
 
-    ##############################################################
-    # Load input images #
-    ##############################################################
+        ##############################################################
+        # Load input images #
+        ##############################################################
 
-    batch_size = 100
-    input_file_names = tf.placeholder(tf.string, shape=[None],
-                                      name='input_file_names')
+        batch_size = FLAGS.batch_size
+        input_file_names = tf.placeholder(tf.string, shape=[None],
+                                          name='input_file_names')
 
-    # Reads an image from a file
-    # decodes it into a dense tensor, and resizes it
-    # to a fixed shape.
-    def _parse_function(filename):
-      image_string = tf.read_file(filename)
-      image_decoded = tf.image.decode_jpeg(image_string, channels=3)
-      image_resized = fixed_image_size_preprocessing_fn(image_decoded)
-      return image_resized
-    dataset = tf.contrib.data.Dataset.from_tensor_slices(input_file_names)
-    dataset = dataset.map(_parse_function)
-    batched_dataset = dataset.batch(batch_size)
-    iterator = batched_dataset.make_initializable_iterator()
-    next_batch = iterator.get_next(name='infer_input')
+        # Reads an image from a file
+        # decodes it into a dense tensor, and resizes it
+        # to a fixed shape.
+        def _parse_function(filename):
+            image_string = tf.read_file(filename)
+            image_decoded = tf.image.decode_jpeg(image_string, channels=3)
+            image_resized = fixed_image_size_preprocessing_fn(image_decoded)
+            return image_resized
 
-    ####################
-    # Define the model #
-    ####################
-    # logits, _ = network_fn(next_batch)
-    pre_logit, _ = network_fn(next_batch)
-    pre_logit = tf.squeeze(pre_logit)
-    tf.logging.info("Pre-logit layer: " + str(pre_logit))
-    # predictions = tf.argmax(logits, 1)
+        dataset = tf.contrib.data.Dataset.from_tensor_slices(input_file_names)
+        dataset = dataset.map(_parse_function)
+        batched_dataset = dataset.batch(batch_size)
+        iterator = batched_dataset.make_initializable_iterator()
+        next_batch = iterator.get_next(name='infer_input')
 
-    if tf.gfile.IsDirectory(FLAGS.checkpoint_path):
-      checkpoint_path = tf.train.latest_checkpoint(FLAGS.checkpoint_path)
-    else:
-      checkpoint_path = FLAGS.checkpoint_path
+        ####################
+        # Define the model #
+        ####################
+        # logits, _ = network_fn(next_batch)
+        pre_logit, _ = network_fn(next_batch)
+        tf.logging.info("Pre-logit layer: " + str(pre_logit))
 
-    tf.logging.info('Inferring using model from %s' % checkpoint_path)
+        pre_logit = tf.squeeze(pre_logit)
 
-    file_name_list = glob.glob(os.path.join(FLAGS.input_dir, '*'))
+        if tf.gfile.IsDirectory(FLAGS.checkpoint_path):
+            checkpoint_path = tf.train.latest_checkpoint(FLAGS.checkpoint_path)
+        else:
+            checkpoint_path = FLAGS.checkpoint_path
 
-    file_name_iter = itertools.imap(
-        None, *([iter(file_name_list)] * batch_size))
-    last_batch_size = len(file_name_list) % batch_size
+        tf.logging.info('Inferring using model from %s' % checkpoint_path)
 
-    # r_server = redis.StrictRedis(host='localhost', port=6379, db=0)
-    saver = tf.train.Saver()
-    with tf.Session() as sess:
-      saver.restore(sess, checkpoint_path)
-      tf.logging.info('model restored')
+        file_name_list = glob.glob(os.path.join(FLAGS.input_dir, '*'))
 
-      for file_names in file_name_iter:
-        sess.run(iterator.initializer, feed_dict={input_file_names:
-                                                  file_names})
-        # outputs = sess.run(predictions, feed_dict={input_file_names:
-        #                                            file_names})
-        outputs = sess.run(pre_logit, feed_dict={input_file_names:
-                                                   file_names})
+        file_name_iter = itertools.imap(
+            None, *([iter(file_name_list)] * batch_size))
+        last_batch_size = len(file_name_list) % batch_size
 
-        image_ids = [os.path.splitext(os.path.basename(file_name))[0] for
-                     file_name in file_names]
-        outputs = outputs.tolist()
-        # mappings = dict(zip(image_ids, outputs))
-        tf.logging.info(np.array(outputs).shape)
-        # tf.logging.info(mappings)
+        # r_server = redis.StrictRedis(host='localhost', port=6379, db=0)
+        results = []
+        saver = tf.train.Saver()
+        with tf.Session() as sess:
+            saver.restore(sess, checkpoint_path)
+            tf.logging.info('model restored')
 
-        # r_server.mset(mappings)
+            for file_names in file_name_iter:
+                sess.run(iterator.initializer, feed_dict={input_file_names:
+                                                              file_names})
+                outputs = sess.run(pre_logit, feed_dict={input_file_names:
+                                                             file_names})
 
-      tf.logging.info('evaluating last batch')
-      # evaluate last batch
-      file_names = file_name_list[-last_batch_size:]
-      sess.run(iterator.initializer, feed_dict={input_file_names:
-                                                file_names})
-      # outputs = sess.run(predictions, feed_dict={input_file_names:
-      #                                            file_names})
-      outputs = sess.run(pre_logit, feed_dict={input_file_names:
-                                                 file_names})
+                image_ids = [os.path.splitext(os.path.basename(file_name))[0] for
+                             file_name in file_names]
+                outputs = outputs.tolist()
 
-      image_ids = [os.path.splitext(os.path.basename(file_name))[0] for
-                   file_name in file_names]
-      outputs = outputs.tolist()
-      # mappings = dict(zip(image_ids, outputs))
-      # tf.logging.info(mappings)
-      # r_server.mset(mappings)
+                results.extend(zip(image_ids, outputs))
+
+            tf.logging.info('evaluating last batch')
+            # evaluate last batch
+            file_names = file_name_list[-last_batch_size:]
+            sess.run(iterator.initializer, feed_dict={input_file_names:
+                                                          file_names})
+            # outputs = sess.run(predictions, feed_dict={input_file_names:
+            #                                            file_names})
+            outputs = sess.run(pre_logit, feed_dict={input_file_names:
+                                                         file_names})
+
+            image_ids = [os.path.splitext(os.path.basename(file_name))[0] for
+                         file_name in file_names]
+            outputs = outputs.tolist()
+
+            results.extend(zip(image_ids, outputs))
+
+    tf.logging.info("%d results in total" % len(results))
+    tags = [r[0] for r in results]
+    X = np.stack([r[1] for r in results], axis=0)
+    tf.logging.info("Writing X shape =" + str(X.shape))
+    pickle.dump({'X': X, 'tags': tags}, open(FLAGS.output_file, 'wb'))
 
 
 if __name__ == '__main__':
-  tf.app.run()
+    tf.app.run()
