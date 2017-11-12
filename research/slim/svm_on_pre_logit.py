a600f39bd0b797ac214f1f7f55635dde9863fa7d
Ziqiang Feng <zf@cs.cmu.edu>
2017-11-11 23:05:08 -0500

    use SGDClassifier() which supports online training of a SVM

--- research/slim/svm_on_pre_logit.py
+++ research/slim/svm_on_pre_logit.py
@@ -3,13 +3,17 @@ import glob
 import os
 import pickle
 import numpy as np
-from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import SGDClassifier
+from sklearn.preprocessing import StandardScaler
 
 DEFAULT_PREFIX = 'pre_logit_'
 
 
-def train(dataset_dir, prefix=DEFAULT_PREFIX, save_model_path=None):
+def train(dataset_dir,
+          prefix=DEFAULT_PREFIX,
+          save_model_path=None,
+          n_iters=20):
     X, y = _load_pre_logit_Xy(dataset_dir, prefix)
 
     X_train, X_validation, y_train, y_validation = train_test_split(X, y, test_size=0.1, random_state=42)
@@ -19,8 +23,11 @@ def train(dataset_dir, prefix=DEFAULT_PREFIX, save_model_path=None):
 
     print X_train[:3], y_train[:3]
 
-    clf = SVC(verbose=True) # TODO grid search hyperparameters
-    clf.fit(X_train, y_train)
+    # scaler = StandardScaler(with_mean=True, with_std=True)
+
+    clf = SGDClassifier(random_state=42, verbose=True)  # TODO grid search hyperparameters
+    for _ in range(n_iters):
+        clf.partial_fit(X_train, y_train, classes=np.array([0, 1]))
 
     print "Finished training"
     print "train accuracy: %f" % clf.score(X_train, y_train)
@@ -30,20 +37,23 @@ def train(dataset_dir, prefix=DEFAULT_PREFIX, save_model_path=None):
         print "saving model to " + save_model_path
         pickle.dump(clf, open(save_model_path, 'wb'))
 
-    # return clf
 
-
-def eval(dataset_dir, checkpoint_path, prefix=DEFAULT_PREFIX):
+def eval(dataset_dir,
+         checkpoint_path,
+         prefix=DEFAULT_PREFIX):
     X_eval, y_eval = _load_pre_logit_Xy(dataset_dir, prefix)
 
     clf = pickle.load(open(checkpoint_path, 'rb'))
-    assert isinstance(clf, SVC)
 
     score = clf.score(X_eval, y_eval)
     return score
 
 
-def retrain(dataset_dir, checkpoint_path, save_model_path=None, prefix=DEFAULT_PREFIX):
+def retrain(dataset_dir,
+            checkpoint_path,
+            save_model_path=None,
+            prefix=DEFAULT_PREFIX,
+            n_iter=10):
     X_new, y_new = _load_pre_logit_Xy(dataset_dir, prefix)
     X_train, X_validation, y_train, y_validation = \
         train_test_split(X_new, y_new, test_size=0.1, random_state=42)
@@ -52,21 +62,22 @@ def retrain(dataset_dir, checkpoint_path, save_model_path=None, prefix=DEFAULT_P
 
     clf = pickle.load(open(checkpoint_path, 'rb'))
 
-    print "before retrain: ", clf.score(X_validation, y_validation)
-    clf.fit(X_train, y_train)
-    print "after retrain: ", clf.score(X_validation, y_validation)
+    print "train accuracy before retrain: ", clf.score(X_train, y_train)
+    print "validation accuracy before retrain: ", clf.score(X_validation, y_validation)
+    for _ in range(n_iter):
+        clf.partial_fit(X_train, y_train)
+    print "train accuracy after retrain: ", clf.score(X_train, y_train)
+    print "validation accuracy after retrain: ", clf.score(X_validation, y_validation)
 
     if save_model_path is not None:
         print "saving model to " + save_model_path
         pickle.dump(clf, open(save_model_path, 'wb'))
 
-    # return clf
-
 
 def _load_pre_logit_Xy(dataset_dir, prefix):
     wildcard = os.path.join(dataset_dir, prefix + '*.p')
     pickle_files = glob.glob(wildcard)
-    print("Loaded %d files: %s" % (len(pickle_files), ','.join(pickle_files)))
+    print("Loaded %d files: \n\t%s" % (len(pickle_files), '\n\t'.join(pickle_files)))
     inputs = [pickle.load(open(f, 'rb')) for f in pickle_files]
     X = np.concatenate([d['X'] for d in inputs])
     y = np.concatenate([d['y'] for d in inputs])
